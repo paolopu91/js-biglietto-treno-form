f55a8c0c9100eedd4ac7bf70ef480a6c271651f4
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-19 17:44:23 +0200

    aggiunte modifiche

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let submitButton = document.getElementById ("submitButton")
+let deleteButton = document.getElementById ("dealteButton")
+const prezzoKm = 0.21;
+const submitButton = document.getElementById("submitButton")
+const deleteButton = document.getElementByI("deleteButton")
+
+deleteButton.addEventListener("click", function(){
+    window.location.reload();
+})
+
+submitButton.addEventListener("click" , function(){
+let userName = document.getElementById("userName");
+let kmDaFare = document.getElementById("kmDaFare");
+let fasciaEta = document.getElementById("fasciaEta");
+
+
+
+
+});
+
+
+
+
+
+
+
