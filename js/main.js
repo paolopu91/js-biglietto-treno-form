3c564c7a6f2e322d31fd0cb322c04c235a8d62ff
paolopu91 <paolo.pugliese1991@gmail.com>
2022-05-20 10:51:39 +0200

    aggiunte modifiche con correzione

--- js/main.js
+++ js/main.js
@@ -1,4 +1,51 @@
+alert("Ciao, compila i dati qui sotto per generare il tuo biglietto")
+
 const submitBtn = document.getElementById("submitButton")
+const inputUserName = document.querySelector("[name='userName']");
+const inputKmDaFare = document.querySelector("[name ='kmDaFare']");
+const inputFasciaEta = document.querySelector("[name ='fasciaEta' ]");
+
+submitBtn.addEventListener("click" , function() {
+
+// document.getElementById("[name = 'userName']").value;
+// document.getElementById("[name='kmDaFare']").value;
+// document.getElementById("[name='fasciaEta']").value;
+
+inputUserName.value = "";
+inputKmDaFare.value = "";
+inputFasciaEta.value = "";
+
+const eta = parseInt(inputUserName.value)
+const km = parseInt(inputKmDaFare.value)
+let datiValidi = true
+
+// if (isNaN(inputFasciaEta) && isNaN(inputKmDaFare)) {
+//     datiValidi=false
+//     alert("Inserisci un numero valido")
+// }
+let prezzoFinale = inputKmDaFare * prezzoKm
+ prezzoFinale.toFixed(2)
+
+if(eta<14) {
+    alert("Torna a casa")
+}
+if(eta<18) {
+   let prezzo20 = prezzo * 0.20
+   prezzo -= prezzo20
+} else if(eta>65) {
+   let prezzo40 = prezzo * 0.4
+    prezzo-= prezzo40
+}
+
+
+
+const userHTML = document.getElementById("userHTML")
+const costoTotaleHTML = document.getElementById("costoTotaleHTML")
+
+userHTML.value
+});
+
+
 const deleteBtn = document.getElementById("deleteButton")
 const prezzoKm = 0.21;
 
@@ -6,13 +53,7 @@ deleteBtn.addEventListener("click", function(){
     window.location.reload();
 })
 
-submitBtn.addEventListener("click" , function(){
-let userName = document.getElementById("userName");
-let kmDaFare = document.getElementById("kmDaFare");
-let fasciaEta = document.getElementById("fasciaEta");
 
-
-});
 
 
 
